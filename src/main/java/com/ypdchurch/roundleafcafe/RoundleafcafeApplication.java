0e01a7b40f7f149fa582d339810fb5e59fffa13a
nhs0912 <nhs0912@nate.com>
2023-12-07 20:32:35 +0900

    fix : add gradle files

---
+++ src/main/java/com/ypdchurch/roundleafcafe/RoundleafcafeApplication.java
@@ -0,0 +1,13 @@
+package com.ypdchurch.roundleafcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundleafcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoundleafcafeApplication.class, args);
+	}
+
+}
