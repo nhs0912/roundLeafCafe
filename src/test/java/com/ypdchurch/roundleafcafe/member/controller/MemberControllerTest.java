6b349a52b97478ef5af0d4a2f5928daa5699fb98
nhs0912 <nhs0912@nate.com>
2024-01-24 12:56:09 +0900

    fix : api rest docs

--- src/test/java/com/ypdchurch/roundleafcafe/member/controller/MemberControllerTest.java
+++ src/test/java/com/ypdchurch/roundleafcafe/member/controller/MemberControllerTest.java
@@ -1,30 +1,30 @@
 package com.ypdchurch.roundleafcafe.member.controller;
 
 import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
-//@AutoConfigureRestDocs
-//@AutoConfigureMockMvc
+@ExtendWith({RestDocumentationExtension.class})
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.roundLeafCafe.com", uriPort = 443)
+@AutoConfigureMockMvc
 @SpringBootTest
 class MemberControllerTest {
 //    @RegisterExtension
@@ -32,14 +32,15 @@ class MemberControllerTest {
 //    @Autowired
 //    protected RestDocumentationResultHandler restDocs;
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
+//    @BeforeEach
+//    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//                .apply(documentationConfiguration(restDocumentation))
+//                .build();
+//    }
 
     @Test
     @DisplayName("api doc test sample")
@@ -60,26 +61,28 @@ class MemberControllerTest {
         json.put("email", "tom@gmail.com");
         json.put("phoneNumber", "01012345678");
 
-        mockMvc.perform(post("/api/join")
+        ResultActions result = mockMvc.perform(post("/api/join")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json.toString()))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isCreated())
-                .andDo(document(
-                        "{class-name}/{method-name}",
+                .andDo(MockMvcResultHandlers.print());
+
+        result.andExpect(status().isCreated())
+                .andDo(document("{class-name}/{method-name}",
                         preprocessRequest(prettyPrint())
                         , preprocessResponse(prettyPrint())
-
-//                , restDocs.document(
-//                                RequestDocumentation.queryParameters(
-//                                        RequestDocumentation.parameterWithName("name").description("이름"),
-//                                        RequestDocumentation.parameterWithName("password").description("password"),
-//                                        RequestDocumentation.parameterWithName("email").description("email"),
-//                                        RequestDocumentation.parameterWithName("phoneNumber").description("phoneNumber")
-//
-//                                )
-//                        ))
+                        , requestFields(
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("password"),
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("email"),
+                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING).description("phoneNumber")
+                        ), responseFields(
+                                fieldWithPath("status").type(JsonFieldType.NUMBER).description("response status"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("message"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("id number"),
+                                fieldWithPath("data.name").type(JsonFieldType.STRING).description("name"),
+                                fieldWithPath("data.email").type(JsonFieldType.STRING).description("email")
+                        )
                 ));
     }
 }
