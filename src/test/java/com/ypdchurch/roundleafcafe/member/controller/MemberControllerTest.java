67bd5c269e78b1ebcc31e6ea5b2135ceb7039649
heeseok.noh <nhs0912@nate.com>
2024-01-27 15:54:21 +0900

    fix : MemberController Join Test Dto로 변경

--- src/test/java/com/ypdchurch/roundleafcafe/member/controller/MemberControllerTest.java
+++ src/test/java/com/ypdchurch/roundleafcafe/member/controller/MemberControllerTest.java
@@ -1,6 +1,7 @@
 package com.ypdchurch.roundleafcafe.member.controller;
 
-import org.json.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ypdchurch.roundleafcafe.member.service.dto.JoinRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,14 +11,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,20 +26,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest
 class MemberControllerTest {
-//    @RegisterExtension
-//    final RestDocumentationExtension restDocumentation = new RestDocumentationExtension ("custom");
-//    @Autowired
-//    protected RestDocumentationResultHandler restDocs;
 
     @Autowired
     private MockMvc mockMvc;
-
-//    @BeforeEach
-//    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-//                .apply(documentationConfiguration(restDocumentation))
-//                .build();
-//    }
 
     @Test
     @DisplayName("api doc test sample")
@@ -54,35 +42,39 @@ class MemberControllerTest {
     @Test
     @DisplayName("멤버 가입 성공 테스트")
     void joinSuccessTest() throws Exception {
+
         //give
-        JSONObject json = new JSONObject();
-        json.put("name", "tom");
-        json.put("password", "1234");
-        json.put("email", "tom@gmail.com");
-        json.put("phoneNumber", "01012345678");
+        JoinRequestDto joinRequestDto = JoinRequestDto.builder()
+                .name("tom")
+                .password("1234")
+                .email("tom@gmail.com")
+                .phoneNumber("01012345678")
+                .build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String joinRequestJson = objectMapper.writeValueAsString(joinRequestDto);
 
         ResultActions result = mockMvc.perform(post("/api/join")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(json.toString()))
+                        .content(joinRequestJson))
                 .andDo(MockMvcResultHandlers.print());
 
         result.andExpect(status().isCreated())
                 .andDo(document("{class-name}/{method-name}",
                         preprocessRequest(prettyPrint())
                         , preprocessResponse(prettyPrint())
-                        , requestFields(
-                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
-                                fieldWithPath("password").type(JsonFieldType.STRING).description("password"),
-                                fieldWithPath("email").type(JsonFieldType.STRING).description("email"),
-                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING).description("phoneNumber")
-                        ), responseFields(
-                                fieldWithPath("status").type(JsonFieldType.NUMBER).description("response status"),
-                                fieldWithPath("message").type(JsonFieldType.STRING).description("message"),
-                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("id number"),
-                                fieldWithPath("data.name").type(JsonFieldType.STRING).description("name"),
-                                fieldWithPath("data.email").type(JsonFieldType.STRING).description("email")
-                        )
+//                        , requestFields(
+//                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+//                                fieldWithPath("password").type(JsonFieldType.STRING).description("password"),
+//                                fieldWithPath("email").type(JsonFieldType.STRING).description("email"),
+//                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING).description("phoneNumber")
+//                        ), responseFields(
+//                                fieldWithPath("status").type(JsonFieldType.NUMBER).description("response status"),
+//                                fieldWithPath("message").type(JsonFieldType.STRING).description("message"),
+//                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("id number"),
+//                                fieldWithPath("data.name").type(JsonFieldType.STRING).description("name"),
+//                                fieldWithPath("data.email").type(JsonFieldType.STRING).description("email")
+//                        )
                 ));
     }
 }
