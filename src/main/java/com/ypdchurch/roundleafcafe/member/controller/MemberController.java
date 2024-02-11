80560dbdcf8689a02e4904296eb376fa8dfef42e
HeeSeok Noh <heeseoknoh@HeeSeoks-Mac-mini.local>
2024-02-11 19:13:48 +0900

    fix : api 문서 Member가입 수정

--- src/main/java/com/ypdchurch/roundleafcafe/member/controller/MemberController.java
+++ src/main/java/com/ypdchurch/roundleafcafe/member/controller/MemberController.java
@@ -1,13 +1,11 @@
 package com.ypdchurch.roundleafcafe.member.controller;
 
+import com.ypdchurch.roundleafcafe.member.controller.dto.JoinRequest;
 import com.ypdchurch.roundleafcafe.member.controller.dto.JoinResponse;
 import com.ypdchurch.roundleafcafe.member.domain.Member;
 import com.ypdchurch.roundleafcafe.member.service.MemberService;
-import com.ypdchurch.roundleafcafe.member.controller.dto.JoinRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,11 +24,12 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public ResponseEntity<JoinResponse> join(@RequestBody JoinRequest joinRequest) {
+    public JoinResponse join(@RequestBody JoinRequest joinRequest) {
 
         log.info("heeseok join.joinRequestDto = {} ", joinRequest);
         Member registeredMember = memberService.registerMember(joinRequest.toEntity(passwordEncoder));
-        JoinResponse joinResponse = new JoinResponse(registeredMember);
-        return new ResponseEntity<JoinResponse>(HttpStatus.CREATED);
+//        JoinResponse joinResponse = new JoinResponse(registeredMember);
+//        return new ResponseEntity<JoinResponse>(HttpStatus.CREATED);
+        return new JoinResponse(registeredMember);
     }
 }
--- src/test/java/com/ypdchurch/roundleafcafe/member/controller/MemberControllerTest.java
+++ src/test/java/com/ypdchurch/roundleafcafe/member/controller/MemberControllerTest.java
@@ -2,6 +2,9 @@ package com.ypdchurch.roundleafcafe.member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ypdchurch.roundleafcafe.member.controller.dto.JoinRequest;
+import com.ypdchurch.roundleafcafe.member.enums.MemberGrade;
+import com.ypdchurch.roundleafcafe.member.enums.MemberRole;
+import com.ypdchurch.roundleafcafe.member.enums.MemberStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,12 +14,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -49,7 +54,11 @@ class MemberControllerTest {
                 .password("1234")
                 .email("tom@gmail.com")
                 .phoneNumber("01012345678")
+                .grade(MemberGrade.NORMAL)
+                .role(MemberRole.CUSTOMER)
+                .status(MemberStatus.ACTIVE)
                 .build();
+
         ObjectMapper objectMapper = new ObjectMapper();
         String joinRequestJson = objectMapper.writeValueAsString(joinRequest);
 
@@ -59,25 +68,23 @@ class MemberControllerTest {
                         .content(joinRequestJson))
                 .andDo(MockMvcResultHandlers.print());
 
-        result.andExpect(status().isCreated())
+        result.andExpect(status().isOk())
                 .andDo(document("{class-name}/{method-name}",
                         preprocessRequest(prettyPrint())
                         , preprocessResponse(prettyPrint())
-//                        , requestFields(
-//                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
-//                                fieldWithPath("password").type(JsonFieldType.STRING).description("password"),
-//                                fieldWithPath("email").type(JsonFieldType.STRING).description("email"),
-//                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING).description("phoneNumber"),
-//                                fieldWithPath("grade").type(JsonFieldType.STRING).description("grade"),
-//                                fieldWithPath("role").type(JsonFieldType.STRING).description("role"),
-//                                fieldWithPath("status").type(JsonFieldType.STRING).description("status")
-//                        ), responseFields(
-//                                fieldWithPath("status").type(JsonFieldType.NUMBER).description("response status"),
-//                                fieldWithPath("message").type(JsonFieldType.STRING).description("message"),
-//                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("id number"),
-//                                fieldWithPath("data.name").type(JsonFieldType.STRING).description("name"),
-//                                fieldWithPath("data.email").type(JsonFieldType.STRING).description("email")
-//                        )
+                        , requestFields(
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("password"),
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("email"),
+                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING).description("phoneNumber"),
+                                fieldWithPath("grade").type(JsonFieldType.STRING).description("grade"),
+                                fieldWithPath("role").type(JsonFieldType.STRING).description("role"),
+                                fieldWithPath("status").type(JsonFieldType.STRING).description("status")
+                        ), responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("id"),
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("email")
+                        )
                 ));
     }
 }
