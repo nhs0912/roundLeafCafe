bb3de917a80c5de8d4217fc6dc35c399fbc4a0c5
HeeSeok Noh <heeseoknoh@HeeSeoks-Mac-mini.local>
2024-02-11 18:43:10 +0900

    feat : add Order domain

--- src/main/java/com/ypdchurch/roundleafcafe/order/domain/Orders.java
+++ src/main/java/com/ypdchurch/roundleafcafe/order/domain/Orders.java
@@ -1,13 +1,15 @@
 package com.ypdchurch.roundleafcafe.order.domain;
 
 import com.ypdchurch.roundleafcafe.common.domain.BaseEntity;
-import io.lettuce.core.dynamic.annotation.CommandNaming;
+import com.ypdchurch.roundleafcafe.order.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
 
 @Getter
 @SuperBuilder
@@ -18,10 +20,22 @@ public class Orders extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = false)
+    @Column(name = "orders_id", updatable = false)
     private Long id;
 
+    @Column(name = "member_id", updatable = false)
+    private Long memberId;
 
+    @Column(name = "basket_id", updatable = false)
+    private Long basketId;
 
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name = "requests")
+    private String requests;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
 
 }
