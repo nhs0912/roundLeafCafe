5dfe984480e3b284308944376b3b0b821f7dc2bf
david Noh <nhs0912@nate.com>
2024-02-18 13:47:07 +0900

    todo : jjwt

--- src/main/java/com/ypdchurch/roundleafcafe/common/auth/jwt/JwtProvider.java
+++ src/main/java/com/ypdchurch/roundleafcafe/common/auth/jwt/JwtProvider.java
@@ -1,17 +1,10 @@
 package com.ypdchurch.roundleafcafe.common.auth.jwt;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.SignatureVerificationException;
-import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.auth0.jwt.interfaces.DecodedJWT;
+
 import com.ypdchurch.roundleafcafe.member.domain.Member;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-
-import static com.auth0.jwt.JWT.require;
-
 
 @Component
 public class JwtProvider {
