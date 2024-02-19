2e7f1b25ef48a1b2a8899dd53a46cf2187fd3b71
david Noh <nhs0912@nate.com>
2024-02-19 18:02:19 +0900

    modify : change Application name

--- src/main/java/com/ypdchurch/roundleafcafe/RoundLeafCafeApplication.java
+++ src/main/java/com/ypdchurch/roundleafcafe/RoundLeafCafeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoundleafcafeApplication {
+public class RoundLeafCafeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RoundleafcafeApplication.class, args);
+		SpringApplication.run(RoundLeafCafeApplication.class, args);
 	}
 
 }
