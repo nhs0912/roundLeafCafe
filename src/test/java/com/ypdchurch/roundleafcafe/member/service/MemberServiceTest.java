31f6d59445969fabba291d4755b42af897bb2a18
david Noh <nhs0912@nate.com>
2024-03-15 16:26:14 +0900

    test : memberService findByEmail test code

--- src/test/java/com/ypdchurch/roundleafcafe/member/service/MemberServiceTest.java
+++ src/test/java/com/ypdchurch/roundleafcafe/member/service/MemberServiceTest.java
@@ -7,6 +7,7 @@ import com.ypdchurch.roundleafcafe.member.domain.Member;
 import com.ypdchurch.roundleafcafe.member.enums.MemberRole;
 import com.ypdchurch.roundleafcafe.member.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.mockito.ArgumentMatchers.any;
@@ -85,6 +87,37 @@ class MemberServiceTest {
         })
                 .isInstanceOf(MemberCustomException.class)
                 .hasMessage(MemberErrorCode.ALREADY_EXIST_EMAIL.getMessage());
+    }
+
+    @Test
+    @DisplayName("멤버 이메일로 찾기 성공 테스트")
+    void findByEmailSuccessTest() {
+        //given
+        JoinRequest joinRequest = JoinRequest.builder()
+                .name("tom")
+                .password("1234")
+                .email("tom@gmail.com")
+                .phoneNumber("01012345678")
+                .build();
+
+        Member requestMember = joinRequest.toEntity(passwordEncoder);
+        //stub1
+        when(memberRepository.findByEmail(any())).thenReturn(Optional.ofNullable(requestMember));
+
+        Member foundMember = memberService.findByEmail("tom@gmail.com");
+        assertThat(foundMember.getEmail()).isEqualTo(requestMember.getEmail());
+    }
+
+    @Test
+    @DisplayName("멤버 이메일로 찾기 실패 테스트")
+    void findByEmailFailTest() {
+        //given
+        //stub1
+        assertThatThrownBy(() -> {
+            memberService.findByEmail("tom@gmail.com");
+        }).isInstanceOf(MemberCustomException.class)
+                .hasMessage(MemberErrorCode.MEMBER_NOT_FOUND.getMessage());
+
     }
 
 }
